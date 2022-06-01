4cd6303b4c3f81b8cc45a03f6aec3d4875985d8d
Fletcher Passow <fletcherpassow@icloud.com>
2022-06-01 00:28:50 +0000

    Add runtime capture

--- recover_loss_trajectories.sh
+++ recover_loss_trajectories.sh
@@ -1,3 +1,4 @@
 find . -name output.log > experiments/output_logs.txt
 xargs -d '\n' grep -i "training summary:" < experiments/output_logs.txt > experiments/training_summaries.txt
 xargs -d '\n' grep -i "validation summary:" < experiments/output_logs.txt > experiments/validation_summaries.txt
+xargs -d '\n' grep -i "total runtime:" < experiments/output_logs.txt > experiments/time_summaries.txt
